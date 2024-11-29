4275790e44f14d87b4309a68eefff6ce19e90c6c
Damian <dvnegrete@gmail.com>
2024-11-28 19:11:30 -0600

    fix: close connection DB for inactivity

--- src/database/config.js
+++ src/database/config.js
@@ -1,14 +1,22 @@
 const mongoose = require("mongoose");
-const { dbUser, dbPassword, dbCluster, dbPort } = require('./../config');
+const { dbUser, dbPassword, dbCluster, dbPort } = require("./../config");
 
 const dbConnection = async () => {
-    try {
-        mongoose.set('strictQuery', false);
-        await mongoose.connect(`mongodb://${dbUser}:${dbPassword}@${dbCluster}:${dbPort}`);
-    } catch (error) {
-        console.error(error);
-        throw new Error('Error starting BD, see LOGS');
-    }
-}
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(
+      `mongodb://${dbUser}:${dbPassword}@${dbCluster}:${dbPort}`,
+      {
+        maxPoolSize: 10,
+        minPoolSize: 0,
+        socketTimeoutMS: 25000,
+        serverSelectionTimeoutMS: 30000,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error starting BD, see LOGS");
+  }
+};
 
-module.exports = { dbConnection }
+module.exports = { dbConnection };
